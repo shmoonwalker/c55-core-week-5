1aab6b492c362eec751c1e849b64ff62d1ea993b
Shadi <sh.moonwalker7@gmail.com>
2026-02-09 22:20:01 +0100

    implement linear and binary search algorithms

--- task-4/bigArray.js
+++ task-4/bigArray.js
@@ -1,7 +1,7 @@
 // you can use this function to generate a big array of the specified size
 // It contains numbers from 0 to size - 1
 // Example: generateBigArray(5) will return [0, 1, 2, 3, 4]
-function generateBigArray(size) {
+export function generateBigArray(size) {
   const array = new Array(size);
   for (let i = 0; i < size; i++) {
     array[i] = i;
--- task-4/search-experiment.js
+++ task-4/search-experiment.js
@@ -1,3 +1,39 @@
-// Write your code here
-// Use generateBigArray to create a big array numbers.
-// Example: generateBigArray(1000000) will create an array of 1 million numbers.
+import { generateBigArray } from "./bigArray.js";
+
+const linearSearch = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+const binarySearch = (arr, target) => {
+  let min = 0;
+  let max = arr.length - 1;
+
+  while (min <= max) {
+    const mid = min + Math.floor((max - min) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      min = mid + 1;
+    } else {
+      max = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+const bigArray = generateBigArray(1000000);
+
+console.time("Linear Search");
+linearSearch(bigArray, 999999);
+console.timeEnd("Linear Search");
+
+console.time("Binary Search");
+binarySearch(bigArray, 999999);
+console.timeEnd("Binary Search");
